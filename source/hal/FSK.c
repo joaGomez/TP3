cf176c036d5d59d73ad8ecfea40f529d464e1de2
Joaco Gomez <joagomez@itba.edu.ar>
2026-06-08 17:42:19 -0300

    Añadi FSK

---
+++ source/hal/FSK.c
@@ -0,0 +1,20 @@
+#include "FSK.h"
+
+#define BUF_SIZE = 22;
+
+uint8_t transm_data[BUF_SIZE];
+uint8_t receive_data[BUF_SIZE];
+
+
+
+
+void saveData() {
+
+
+
+
+    
+}
+
+
+
---
+++ source/hal/FSK.h
@@ -0,0 +1,12 @@
+#ifndef FSK_H
+#define FSK_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+
+
+#endif	// FSK_H
